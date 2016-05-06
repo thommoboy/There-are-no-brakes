d7268d956040190912e6954d1ccc7137ab55521f
Nathan <nathanjbrown@live.com>
2016-05-06 12:46:50 +1000

    old anchor files (need updating)

---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Anchor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anchor : MonoBehaviour{
+	private GameObject anchor;
+	public int minHeight;
+	
+	public void Start(){
+		anchor = GameObject.Find("Anchor");
+	}
+    
+	public void FixedUpdate(){
+		if(anchor.transform.position.y < minHeight){
+			Debug.Log("Anchor fell!");
+			//Level completion stuff here
+		}
+	}
+}
