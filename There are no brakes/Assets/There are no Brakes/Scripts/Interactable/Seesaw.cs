854744e176403879e3b55f0926211d416e1b4f54
thommoboy <lambofgod2009@live.com.au>
2016-05-07 13:49:05 +1000

    Feng's Updated Assets
    7/5/2016
    Updated Feng's Assets

---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Interactable/Seesaw.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Seesaw : MonoBehaviour {
+    public string playerTag = "player";
+    public float bounceForce = 100;
+    List<Collision> touchList;
+    int count;
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        touchList = new List<Collision>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    
+    void OnCollisionEnter(Collision touchItem) {
+        if (touchItem.gameObject.name == "Plane" || touchItem.gameObject.transform.position.y < this.transform.position.y) return;
+
+        if (!touchList.Contains(touchItem)) {
+            touchList.Add(touchItem);
+            count++;
+        }
+        
+
+        for (int i = 0; i < count; i++) {
+            Collision other = touchList[i];
+            if (touchItem.gameObject.transform.position.y < other.gameObject.transform.position.y) continue;
+            bool onSameSide = true;
+            float otherX = other.gameObject.transform.position.x - this.gameObject.transform.position.x;
+            float touchItemX = touchItem.gameObject.transform.position.x - this.gameObject.transform.position.x;
+            if (otherX * touchItemX < 0) { onSameSide = false; }
+            if (onSameSide) continue;
+            
+            other.collider.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce );
+            //string force = (Vector3.up * bounceForce * (Mathf.Abs(otherX) / this.transform.localScale.x) * (Mathf.Abs(touchItemX) / this.transform.localScale.x)).ToString();
+            //print("give force");
+            //print(force);
+
+        }
+
+        string touch = "";
+        for (int i = 0; i < count; i++)
+        {
+            touch += touchList[i].gameObject.name + ", ";
+        }
+        print(touch);
+        
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        touchList.Remove(other);
+        count--;
+    }
+}
---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    public float jumpPower = 1000;
+    bool onAir = true;
+    int touchItem = 0;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey) {
+            if (Input.GetKey(KeyCode.A)) {
+                this.transform.Translate(Vector3.left * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.D)) {
+                this.transform.Translate(Vector3.right * Time.deltaTime);
+            }
+
+        }
+
+        if (Input.anyKeyDown) {
+            if (Input.GetKey(KeyCode.Space) && this.transform.position.y <= 0)
+            {
+                this.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower);
+            }
+        }
+
+	}
+
+    void OnCollisionEnter(Collision other) {
+        touchItem++;
+        onAir = false;
+    }
+
+
+    void OnCollisionExit(Collision other) {
+        touchItem--;
+        if (touchItem == 0) onAir = true;
+    }
+}
