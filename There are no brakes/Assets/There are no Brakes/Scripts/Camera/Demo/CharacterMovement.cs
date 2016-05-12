b97021b1237ab7d86dbfb7453b69157d0b03e1f7
thommoboy <lambofgod2009@live.com.au>
2016-05-12 22:09:24 +1000

    Last push for tonight
    Last push for tonight

--- There are no brakes/Assets/There are no Brakes/Scripts/Camera/Demo/CharacterMovement.cs
+++ There are no brakes/Assets/There are no Brakes/Scripts/Camera/Demo/CharacterMovement.cs
@@ -11,36 +11,28 @@ public class CharacterMovement : MonoBehaviour {
 	/// movement force for the object, more force will move it faster.
 	/// Remember to tweak this depending of the mass of your rigid body.
 	/// </summary>
-	public float movementForce = 10; 
-	
-	
-	private float horizontal;
-	private float vertical;
+	public float movementForce = 0.5f; 
 
 	public float jumpForce = 10;
 	
 	// Use this for initialization
-	void Start () {		
-		
+	void Start () {
 	}
 	
 	
 	void FixedUpdate()
 	{
-		
-	// WE move the rigid body depending of the input.	
-		if (horizontal < 0)
-		{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		if (horizontal < 0){
 			transform.rotation = Quaternion.Euler (0, 270, 0);
-			GetComponent<Rigidbody>().AddForce(GetComponent<Rigidbody>().rotation * Vector3.left * movementForce);
 		}
-		
-		if (horizontal > 0)
-		{
+		if (horizontal > 0){
 			transform.rotation = Quaternion.Euler (0, 90, 0);
-			GetComponent<Rigidbody>().AddForce(GetComponent<Rigidbody>().rotation * Vector3.left * movementForce);
 		}
-		
+
+		GetComponent<Rigidbody> ().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, GetComponent<Rigidbody>().velocity.y, horizontal * movementForce);
+		//WE move the rigid body depending of the input.
 //		if (vertical < 0)
 //		{
 //			GetComponent<Rigidbody>().AddForce(GetComponent<Rigidbody>().rotation * Vector3.back * movementForce);
@@ -55,19 +47,14 @@ public class CharacterMovement : MonoBehaviour {
 		RaycastHit hit;
 		Ray downRay = new Ray (transform.position, -Vector3.up);
 		Physics.Raycast (downRay, out hit);
-		Debug.Log (hit.distance);
+		//Debug.Log (hit.distance);
+
 		if (Input.GetButton ("Jump") && hit.distance < 1.1) {
 			GetComponent<Rigidbody> ().velocity = new Vector3 (GetComponent<Rigidbody>().velocity.x, jumpForce, GetComponent<Rigidbody>().velocity.z);
 		}
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		// we register the input so the fixed update knows what to do with the rigid body.
-		horizontal = Input.GetAxis("Horizontal");
-		vertical = Input.GetAxis("Vertical");		
-		
 	}
 }
