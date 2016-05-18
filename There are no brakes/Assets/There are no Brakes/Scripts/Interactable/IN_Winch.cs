66f74b1594ed70b5a41c650a3a602b9e3a2a2d8f
Nathan <nathanjbrown@live.com>
2016-05-18 21:06:01 +1000

    added button prompt and sound effect to winch

--- There are no brakes/Assets/There are no Brakes/Scripts/Interactable/IN_Winch.cs
+++ There are no brakes/Assets/There are no Brakes/Scripts/Interactable/IN_Winch.cs
@@ -6,17 +6,37 @@ public class IN_Winch : IN_InteractableObject{
 	void Start(){
 		targetminheight = target.transform.position.y;
 	}
+	
+	private bool playingsound = false;
+	void playsoundeffect(){
+		if(!playingsound){
+			GetComponent<AudioSource>().Play();
+			playingsound = true;
+		}
+	}
 
 	public GameObject target;
 	public bool gate = false;
+	private bool intrigger = false;
 	void OnTriggerStay(Collider other) {
 		if(other.tag == "Player"){
+			intrigger = true;
 			if(Input.GetKey(KeyCode.S)){
 				WindWinch();
-			}
-			if(Input.GetKey(KeyCode.DownArrow)) {
+			} else if(Input.GetKey(KeyCode.DownArrow)) {
 				WindWinch();
+			} else {
+				GetComponent<AudioSource>().Stop();
+				playingsound = false;
 			}
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if(other.tag == "Player"){
+			intrigger = false;
+			GetComponent<AudioSource>().Stop();
+			playingsound = false;
 		}
 	}
 	
@@ -24,6 +44,7 @@ public class IN_Winch : IN_InteractableObject{
 	public float movespeed = 1.5f;
 	private bool up = true;
 	void WindWinch(){
+		playsoundeffect();
 		if(gate){
 			if(target.transform.rotation.z < 0.5f){
 				target.transform.Rotate(0, 0, Time.deltaTime*25);
@@ -42,6 +63,13 @@ public class IN_Winch : IN_InteractableObject{
 				}
 			}
 			target.transform.localPosition = Vector3.Lerp(target.transform.localPosition, platpos, Time.deltaTime);
+		}
+	}
+	
+	
+	void OnGUI(){
+		if(intrigger){
+			GUI.Label(new Rect (Screen.width/2 - 170, Screen.height/2 - 50, 500, 50), "Press [Down] to use");
 		}
 	}
 }
