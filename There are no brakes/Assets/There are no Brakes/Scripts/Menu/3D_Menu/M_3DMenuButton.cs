60b5eb23492a61f14fe34b28b465894f6d7ccc66
BlackFuse <xinyu.feng1995@gmail.com>
2016-05-19 18:58:18 +1000

    new Main menu

---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Menu/3D_Menu/M_3DMenuButton.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class M_3DMenuButton : MonoBehaviour {
+    public GameObject[] MenuButtonList;
+	public GameObject[] MenuList;
+	private int[] MenuMoveStatu;
+    private Vector3 button_ori_scale;
+    private int[] sizeStatu;
+    private float resizeSpeed = 1.03f;
+    private int focusedButtonIndex = -1;
+	private Vector3 menu_ori_position;
+	bool right_menu = false;
+	float menuMoveSpeed  = 20;
+    // Use this for initialization
+    void Start () {
+		//init menu movement statu and button size statu
+		//0: stay
+		//1: go up / being bigger
+		//-1: go down // being smaller
+		//2: go up until not in screen
+		MenuMoveStatu = new int[MenuList.Length];
+		for (int i = 0; i < MenuMoveStatu.Length; i++) {
+			MenuMoveStatu[i] = 0;
+        }
+		sizeStatu = new int[MenuButtonList.Length];
+		for (int i = 0; i < sizeStatu.Length; i++) {
+			sizeStatu[i] = 0;
+		}
+
+		menu_ori_position = MenuList [0].transform.position;
+        button_ori_scale = MenuButtonList[0].transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		GameObject hitThing;
+		if (focusedButtonIndex != -1) {
+			sizeStatu [focusedButtonIndex] = -1;
+		}
+		if (Physics.Raycast (ray, out hit, 1000.00f)) {
+			hitThing = hit.collider.gameObject;
+			if (hitThing.tag == "MenuButton") {
+				ButtonOnSelected (hitThing);
+				if (Input.GetKeyDown (KeyCode.Mouse0)) {
+					onClick (hitThing.name);
+				}
+			}
+
+		}
+		updateButton ();
+		updateMenu ();
+
+    }
+
+	void updateMenu(){
+		for (int i = 0; i < MenuList.Length; i++) {
+			if (MenuMoveStatu [i] == 1) {
+				MenuList[i].transform.position = MenuList[i].transform.position + Vector3.up * menuMoveSpeed ;
+				if (MenuList [i].transform.position.y > 250) {
+					MenuMoveStatu [i] = 0;
+				}
+			}
+
+			if (MenuMoveStatu [i] == -1) {
+				MenuList[i].transform.position = MenuList[i].transform.position - Vector3.up * menuMoveSpeed;
+				if (MenuList [i].transform.position.y < 170) {
+					MenuMoveStatu [i] = 1;
+				}
+			}
+
+			if (MenuMoveStatu [i] == 2) {
+				MenuList[i].transform.position = MenuList[i].transform.position + Vector3.up * menuMoveSpeed;
+				if (MenuList [i].transform.position.y > 700) {
+					MenuMoveStatu [i] = 1;
+				}
+			}
+		}
+	}
+
+	void onClick(string buttonName){
+		if (buttonName == "MenuButton_Start") {
+			updateMenuStatu (0);
+		}
+		if (buttonName == "MenuButton_Option") {
+			updateMenuStatu (1);
+		}
+		if (buttonName == "MenuButton_Credits") {
+			updateMenuStatu (2);
+		}
+		if (buttonName == "MenuButton_Exit") {
+			Application.Quit ();
+		}
+	}
+
+	void updateMenuStatu(int menuIndex){
+		if (MenuList [menuIndex].transform.position.y < 700 || MenuMoveStatu[menuIndex] != 0) {
+			return;
+		}
+		MenuMoveStatu [menuIndex] = -1;
+		if (right_menu) {
+			MenuList [menuIndex].transform.position = MenuList [menuIndex].transform.position + Vector3.up * 450;
+		}
+		if (right_menu) {
+			for (int i = 0; i < MenuList.Length; i++) {
+				if (i == menuIndex)
+					continue;
+				if (MenuList [i].transform.position.y < 600) {
+					MenuMoveStatu [i] = 2;
+					break;
+				}
+			}
+		}
+		right_menu = true;
+	}
+
+
+	void updateButton(){
+		for (int i = 0; i < MenuButtonList.Length; i++)
+		{
+			if (sizeStatu[i] == 1)
+			{
+				if (MenuButtonList[i].transform.localScale.x < button_ori_scale.x * 1.3)
+				{
+					MenuButtonList[i].transform.localScale = MenuButtonList[i].transform.localScale * resizeSpeed;
+				}
+				else { sizeStatu[i] = 0; }
+			}
+
+			if (sizeStatu[i] == -1){
+				if (MenuButtonList[i].transform.localScale.x > button_ori_scale.x)
+				{
+					MenuButtonList[i].transform.localScale = MenuButtonList[i].transform.localScale * (1/resizeSpeed);
+				}
+				else { sizeStatu[i] = 0; }
+			}
+		}
+	}
+
+    void ButtonOnSelected(GameObject button) {
+        for (int i = 0; i < MenuButtonList.Length; i++)
+        {
+            if (MenuButtonList[i] == button) {
+                focusedButtonIndex = i;
+                sizeStatu[i] = 1;
+            }
+        }
+    }
+
+    void MouseOnOver(GameObject overButton) {
+        
+    }
+		
+}
---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Menu/3D_Menu/M_3D_OnMouseOver.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class M_3D_OnMouseOver : MonoBehaviour {
+	Vector3 ori_Scale;
+	public Slider loadingBar;
+	public  GameObject loadingImage;
+	private AsyncOperation async;
+	// Use this for initialization
+	void Start () {
+		ori_Scale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		GameObject hitThing;
+		if (Physics.Raycast (ray, out hit, 1000.00f)) {
+			hitThing = hit.collider.gameObject;
+			if (hitThing == this.gameObject) {
+				transform.localScale = ori_Scale * 1.3f;
+				if (Input.GetKeyDown (KeyCode.Mouse0)) {
+					ClickAsync (1);
+				}
+			} else {
+				transform.localScale = ori_Scale;
+			}
+		} 
+	}
+
+	public void ClickAsync(int level){
+		loadingImage.SetActive (true);
+		StartCoroutine (LoadLevel (level));
+	}
+
+	IEnumerator LoadLevel(int level)
+	{
+		async = Application.LoadLevelAsync (level);
+		while (!async.isDone) {
+			loadingBar.value = async.progress;
+			yield return null;
+		}
+
+	}
+}
