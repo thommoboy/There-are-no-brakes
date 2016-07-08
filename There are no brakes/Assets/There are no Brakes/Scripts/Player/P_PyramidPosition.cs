564841d67e678dd3819fe18cc982e30a62c81f05
Nathan <nathanjbrown@live.com>
2016-07-08 16:45:39 +1000

    pyramid position calculator update
    can now work with a pyramid of any size and 'steepness'
    (though having the pyramid off-center in the map will rek it)

--- There are no brakes/Assets/There are no Brakes/Scripts/Player/P_PyramidPosition.cs
+++ There are no brakes/Assets/There are no Brakes/Scripts/Player/P_PyramidPosition.cs
@@ -7,26 +7,34 @@ using UnityEngine;
 using System.Collections;
  
 public class P_PyramidPosition : MonoBehaviour {
+	// Direction from player to players camera eg if player moves along z axis it will be x+ or x-
 	public string Direction = "x+";
-	private int PryamidWidth = 53;
+	// HALF of pyramids actual width aka 'radius'
+	private int PryamidWidth = 52;
+	// Height of pyramid from base to point
+	private float PyramidHeight = 52;
+	private float RelativeAdjust;
 	private Vector3 PlayerPos;
 	private float PosUpdate;
 	
 	void Start(){
+		RelativeAdjust = PryamidWidth / PyramidHeight;
 	}
 	
 	void Update(){
 		PlayerPos = this.transform.position;
 		if(Direction == "x+"){
-			PosUpdate = PryamidWidth - PlayerPos.y;
+			PosUpdate = PryamidWidth - (PlayerPos.y*RelativeAdjust);
 			this.transform.position = new Vector3(PosUpdate,PlayerPos.y,PlayerPos.z);
 		} else if(Direction == "x-"){
-			
+			PosUpdate =  (PlayerPos.y*RelativeAdjust) - PryamidWidth;
+			this.transform.position = new Vector3(PosUpdate,PlayerPos.y,PlayerPos.z);
 		} else if(Direction == "z+"){
-			PosUpdate = PryamidWidth - PlayerPos.y;
+			PosUpdate = PryamidWidth - (PlayerPos.y*RelativeAdjust);
 			this.transform.position = new Vector3(PlayerPos.x,PlayerPos.y,PosUpdate);
 		} else {
-			
+			PosUpdate =  (PlayerPos.y*RelativeAdjust) - PryamidWidth;
+			this.transform.position = new Vector3(PlayerPos.x,PlayerPos.y,PosUpdate);
 		}
 	}
 }
