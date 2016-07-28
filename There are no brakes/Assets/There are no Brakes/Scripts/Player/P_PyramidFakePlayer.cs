008eccc52884f30d08ce7ac9a308e6988479ebee
Nathan <nathanjbrown@live.com>
2016-07-28 11:54:58 +1000

    updates to pyramid test level
    orthographic camera test, fake players to test shared view

---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Player/P_PyramidFakePlayer.cs
@@ -0,0 +1,25 @@
+﻿/***********************
+ * P_PyramidFakePlayer.cs
+ * Originally Written by Nathan Brown
+ * Modified By:
+ ***********************/
+using UnityEngine;
+using System.Collections;
+ 
+public class P_PyramidFakePlayer : MonoBehaviour {
+	// script just to test out pyramid level with only 1 controllable player
+	private Vector3 PlayerPOS;
+	private Vector3 thisPOS;
+	
+	void Start(){
+	}
+	
+	void Update(){
+		PlayerPOS = GameObject.Find("Player1").transform.position;
+		if(this.name == "FAKEplayer3"){
+			this.transform.position = new Vector3(-PlayerPOS.z,PlayerPOS.y,PlayerPOS.x);
+		} else {
+			this.transform.position = new Vector3(PlayerPOS.z,PlayerPOS.y,-PlayerPOS.x);
+		}
+	}
+}
