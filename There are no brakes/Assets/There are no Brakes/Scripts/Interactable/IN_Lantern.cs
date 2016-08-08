2115c4648a4c22382ccca8dd745f250ac8b4d8cd
Nathan <nathanjbrown@live.com>
2016-08-08 20:45:59 +1000

    Industrial level Lighting update
    Worked out how to light up the outside without lighting up the inside,
    also fixed the lantern script

--- There are no brakes/Assets/There are no Brakes/Scripts/Interactable/IN_Lantern.cs
+++ There are no brakes/Assets/There are no Brakes/Scripts/Interactable/IN_Lantern.cs
@@ -1,7 +1,8 @@
 ﻿/***********************
- * IN_Activation.cs
- * Originally Written by Nathan Brown
+ * IN_Lantern.cs
+ * Originally Written by Joshua
  * Modified By:
+	Nathan Brown (Bug fixes)
  ***********************/
 using UnityEngine;
 using System.Collections;
@@ -9,88 +10,30 @@ using System.Collections;
 public class IN_Lantern : MonoBehaviour{
 	public bool activated = false;
 	public GameObject lantern;
-	//public bool lever = false;
-	//public bool pressureplate = false;
-	//public bool lantern = false;
-	private float timeout = 0.25F;
-	private float nextInteract = 0.0F;
 	
 	void Start(){
-		lantern.GetComponent<Light> ().enabled = false;
+		lantern.GetComponent<Light>().enabled = false;
 	}
 	
 	private bool intrigger = false;
-	void OnTriggerEnter(Collider other) {
-//		if(lever){
-//			if(other.tag == "Player"){
-//				intrigger = true;
-//				if(Time.time > nextInteract){
-//					if (other.name == "Player1"){
-//						if (Input.GetKey (KeyCode.DownArrow)) {
-//							changeState();
-//						}
-//					}
-//					if (other.name == "Player2"){
-//						if (Input.GetKey (KeyCode.S)) {
-//							changeState();
-//						}
-//					}
-//					if (other.name == "Player3"){
-//						if (Input.GetKey (KeyCode.K)) {
-//							changeState();
-//						}
-//					}
-//				}
-//			}
-//		}
-		if(/*lantern &&*/ !activated){
-			if(other.tag == "Player"){
-				intrigger = true;
-				if (other.name == "Player1"){
-					//if (Input.GetKey (KeyCode.DownArrow)) {
-						//Debug.Log ("You pressed down!");
-						changeState();
-						lantern.GetComponent<Light> ().enabled = true;
-					//}
-				}
-				if (other.name == "Player2"){
-					//if (Input.GetKey (KeyCode.S)) {
-						changeState();
-						lantern.GetComponent<Light> ().enabled = true;
-					//}
-				}
-				if (other.name == "Player3"){
-					//if (Input.GetKey (KeyCode.K)) {
-						changeState();
-						lantern.GetComponent<Light> ().enabled = true;
-					//}
-				}
+	void OnTriggerStay(Collider other) {
+		if (other.name == "Player3"){
+			intrigger = true;
+			if (Input.GetKey (KeyCode.K)) {
+				lantern.GetComponent<Light> ().enabled = true;
 			}
 		}
-		/*if(pressureplate){
-			if(other.tag == "Player" || other.tag == "Weight"){
-				activated = true;
-				this.transform.localScale = new Vector3(3, 2, 3);
-			}
-		}*/
 	}
 	
-	void OnTriggerExit(Collider other) {
-		if(other.tag == "Player" || other.tag == "Weight"){
+	void OnTriggerExit(Collider other){
+		if (other.name == "Player3"){
 			intrigger = false;
 		}
 	}
 	
-	void changeState(){
-		activated = !activated;
-		nextInteract = Time.time + timeout;
-//		if(lever){this.transform.Rotate(0, 180, 0);}
-//		if(lantern){intrigger = false;}
-	}
-	
 	
 	void OnGUI(){
-		if(intrigger){
+		if(intrigger && !activated){
 			GUI.Label(new Rect (Screen.width/2 - 170, Screen.height/2 - 50, 500, 50), "Press [Interact] to use");
 		}
 	}
