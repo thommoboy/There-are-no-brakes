1515b784655e549b58996ea651c67a5f03e1393e
dantesantana <wolfgang.101@hotmail.com>
2016-08-11 11:12:27 +1000

    Minor Lighting Fix
    Dimmed lights, increased industrialist light range

--- There are no brakes/Assets/There are no Brakes/Scripts/Player/P_PlayerLight.cs
+++ There are no brakes/Assets/There are no Brakes/Scripts/Player/P_PlayerLight.cs
@@ -21,11 +21,11 @@ public class P_PlayerLight : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (light.name == "Light1") {
-			light.transform.position = new Vector3(player1.transform.position.x+0.5f,player1.transform.position.y,player1.transform.position.z);
+			light.transform.position = new Vector3(player1.transform.position.x+1.0f,player1.transform.position.y+0.5f,player1.transform.position.z);
 		} else if(light.name == "Light2"){
-			light.transform.position = new Vector3(player2.transform.position.x+0.5f,player2.transform.position.y,player2.transform.position.z);;
+			light.transform.position = new Vector3(player2.transform.position.x+1.0f,player2.transform.position.y+0.5f,player2.transform.position.z);;
 		} else if(light.name == "Light3"){
-			light.transform.position = new Vector3(player3.transform.position.x+0.5f,player3.transform.position.y,player3.transform.position.z);;
+			light.transform.position = new Vector3(player3.transform.position.x+1.0f,player3.transform.position.y+0.5f,player3.transform.position.z);;
 			light.transform.rotation = Quaternion.LookRotation (-player3.transform.right); //Industrialist
 		}
 	}
