2ee8a413b11e4471ec4dd4869b0de7d45b18eec5
dantesantana <wolfgang.101@hotmail.com>
2016-08-21 15:11:00 +1000

    Tutorial Industrial Lighting
    Added lighting to the Industrial section of the tutorial, still need to
    add player lights and replace stuff with Ashleighs work!

---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Scene/Tutorial_FlickerLight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial_FlickerLight : MonoBehaviour {
+	private Light light;
+	private float flickerSpeed = 1.0f;
+	private float targetIntensity = 8.0f;
+	// Use this for initialization
+	void Start () {
+		light = this.GetComponent<Light> ();
+		light.intensity = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.intensity = Mathf.Lerp (light.intensity, targetIntensity, flickerSpeed*Time.deltaTime);
+		if (light.intensity > 6) {
+			targetIntensity = Random.Range (0.0f, 0.5f);
+		} else if (light.intensity < 2) {
+			targetIntensity = Random.Range (6.5f, 8.0f);
+		}
+	}
+}
