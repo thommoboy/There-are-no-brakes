5ac5b0bf3d85a5f4a90c56688107d5c9022538e8
Nathan <nathanjbrown@live.com>
2016-08-25 00:27:59 +1000

    cutscene
    works in editor, hope it still works with exe build

---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Scene/CutscenePlay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutscenePlay : MonoBehaviour {
+	public MovieTexture cutscene;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = cutscene as MovieTexture;
+		cutscene.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!cutscene.isPlaying){
+		Application.LoadLevel ("Tutorial Level");
+		}	
+	}
+}
