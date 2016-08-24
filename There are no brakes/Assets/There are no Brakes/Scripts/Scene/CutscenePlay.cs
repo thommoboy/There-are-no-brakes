1f5bb6fadfff551f45a908c7dffd5765bdf6117d
Nathan <nathanjbrown@live.com>
2016-08-25 09:23:20 +1000

    re-fix of lighting bug
    there was similar bug as from industrial to pyramid except happening
    from cutscene to tutorial which was fixed b4 but got reverted

--- There are no brakes/Assets/There are no Brakes/Scripts/Scene/CutscenePlay.cs
+++ There are no brakes/Assets/There are no Brakes/Scripts/Scene/CutscenePlay.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class CutscenePlay : MonoBehaviour {
 	public MovieTexture cutscene;
-    WWW request;
-    string url;
+    //WWW request;
+    //string url;
     // Use this for initialization
     void Start () {
-        url = "file:///" + Application.dataPath + "/Resources/Cutscene.ogv";
-        request = new WWW(url);
+        //url = "file:///" + Application.dataPath + "/Resources/Cutscene.ogv";
+        //request = new WWW(url);
         //cutscene = (MovieTexture)Resources.Load("Cutscene.ogv",MovieTexture);
-        cutscene = (MovieTexture)request.movie;
+        //cutscene = (MovieTexture)request.movie;
 		GetComponent<Renderer>().material.mainTexture = cutscene as MovieTexture;
 		cutscene.Play();
 
