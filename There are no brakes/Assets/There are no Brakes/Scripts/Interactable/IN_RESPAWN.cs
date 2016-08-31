a61a558fa9c1318841a285a0942ae1cd5fb55e98
Nathan <nathanjbrown@live.com>
2016-08-31 22:50:05 +1000

    fixed tutorial respawn
    now there is a spawn point either side of the gap and entering the
    trigger of either will update a players respawn position

--- There are no brakes/Assets/There are no Brakes/Scripts/Interactable/IN_RESPAWN.cs
+++ There are no brakes/Assets/There are no Brakes/Scripts/Interactable/IN_RESPAWN.cs
@@ -7,40 +7,45 @@ using UnityEngine;
 using System.Collections;
 
 public class IN_RESPAWN : MonoBehaviour {
-	public bool isTutorial = false;
-	public GameObject spawnPoint;
-	private Vector3 P1respawnPOS;
-	private Vector3 P2respawnPOS;
-	private Vector3 P3respawnPOS;
+	public bool isSpawnPoint = false;
+	static Vector3 P1respawnPOS;
+	static Vector3 P2respawnPOS;
+	static Vector3 P3respawnPOS;
 	public bool destroyObjects = false;
 
 	void Start () {
 		P1respawnPOS = GameObject.Find("Player1").transform.position;
 		P2respawnPOS = GameObject.Find("Player2").transform.position;
 		P3respawnPOS = GameObject.Find("Player3").transform.position;
-		if (isTutorial) {
-			P1respawnPOS = spawnPoint.transform.position;
-			P2respawnPOS = spawnPoint.transform.position;
-			P3respawnPOS = spawnPoint.transform.position;
-		}
-	}
-	
-	void Update () {
 	}
 	
 	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Player") {
+		// if its a damaging object respawn the player
+		if(!isSpawnPoint){
+			if (other.tag == "Player") {
+				if (other.name == "Player1") {
+					other.transform.position = P1respawnPOS;
+				}
+				if (other.name == "Player2") {
+					other.transform.position = P2respawnPOS;
+				}
+				if (other.name == "Player3") {
+					other.transform.position = P3respawnPOS;
+				}
+			} else if (other.tag == "Weight" && destroyObjects) {
+				Destroy (other.gameObject);
+			}
+		// if its a spawn point update the players spawn position
+		} else {
 			if (other.name == "Player1") {
-				other.transform.position = P1respawnPOS;
+				P1respawnPOS = new Vector3 (other.transform.position.x, this.transform.position.y, this.transform.position.z);
 			}
 			if (other.name == "Player2") {
-				other.transform.position = P2respawnPOS;
+				P2respawnPOS = new Vector3 (other.transform.position.x, this.transform.position.y, this.transform.position.z);
 			}
 			if (other.name == "Player3") {
-				other.transform.position = P3respawnPOS;
+				P3respawnPOS = new Vector3 (other.transform.position.x, this.transform.position.y, this.transform.position.z);
 			}
-		} else if (other.tag == "Weight" && destroyObjects) {
-			Destroy (other.gameObject);
 		}
 	}
 }
