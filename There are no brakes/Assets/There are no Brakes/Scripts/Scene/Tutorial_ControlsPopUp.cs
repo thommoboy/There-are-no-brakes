933de3ec74da0e7cdd110a71cbbe1cbe1dc71e9a
dantesantana <wolfgang.101@hotmail.com>
2016-08-31 12:41:17 +1000

    Text Pop Up Modifications with Nathan
    Implemented Basic control mapping at beginning of Tute rooms

---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Scene/Tutorial_ControlsPopUp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial_ControlsPopUp : MonoBehaviour {
+
+	public GameObject PopUp;
+	private bool activated = false;
+	private Vector3 camPos;
+	// Use this for initialization
+	void Start () {
+		camPos = GameObject.Find ("GUI Camera").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerStay(Collider other) {			
+		if (other.tag == "Player" && !activated) {
+			if (Input.GetKey("return")) {
+				activated = true;
+				PopUp.SetActive(false);
+			}
+			PopUp.transform.position = new Vector3(camPos.x - 15, camPos.y, camPos.z);
+		}
+	}
+}
