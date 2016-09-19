6ddc95d3abe776238ffe45e2d138ba19708729ce
Nathan <nathanjbrown@live.com>
2016-09-19 21:55:14 +1000

    train wheels spin now

---
+++ There are no brakes/Assets/There are no Brakes/Scripts/Interactable/Train_Wheels.cs
@@ -0,0 +1,18 @@
+﻿/***********************
+ * Train_Wheels.cs
+ * Originally Written by Nathan Brown
+ * Modified By:
+ ***********************/
+ using UnityEngine;
+using System.Collections;
+
+public class Train_Wheels : MonoBehaviour {
+    private float speed = 999;
+    // Use this for initialization
+    void Start () {
+    }
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(Vector3.down * Time.deltaTime * speed);
+    }
+}
