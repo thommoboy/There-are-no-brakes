a34b802461a11cb28ce784934f40a0531e5d42b9
Nathan <nathanjbrown@live.com>
2016-09-30 21:49:30 +1000

    update to player visibility
    players now get an unlit background/border around them when icons appear

--- There are no brakes/Assets/There are no Brakes/Scripts/Interactable/IN_ShowIcon.cs
+++ There are no brakes/Assets/There are no Brakes/Scripts/Interactable/IN_ShowIcon.cs
@@ -14,20 +14,28 @@ public class IN_ShowIcon : MonoBehaviour {
 	private GameObject Icon1;
 	private GameObject Icon2;
 	private GameObject Icon3;
-	private GameObject Icon4;
-	private GameObject Icon5;
-	private GameObject Icon6;
+	private GameObject highlight1;
+	private GameObject highlight2;
+	private GameObject highlight3;
+	private Vector3 player1;
+	private Vector3 player2;
+	private Vector3 player3;
+	private GameObject PlayerController;
 
 	void Start () {
 		Icon1 = GameObject.Find("Icon1");
 		Icon2 = GameObject.Find("Icon2");
 		Icon3 = GameObject.Find("Icon3");
-		Icon4 = GameObject.Find("Icon4");
-		Icon5 = GameObject.Find("Icon5");
-		Icon6 = GameObject.Find("Icon6");
+		highlight1 = GameObject.Find("highlight (1)");
+		highlight2 = GameObject.Find("highlight (2)");
+		highlight3 = GameObject.Find("highlight (3)");
+		player1 = GameObject.Find("Player1").transform.position;
+		player2 = GameObject.Find("Player2").transform.position;
+		player3 = GameObject.Find("Player3").transform.position;
 		timer1 = 0;
 		timer2 = 0;
 		timer3 = 0;
+		PlayerController = GameObject.Find ("PlayerControllers");
 	}
 	
 	void Update () {
@@ -45,25 +53,68 @@ public class IN_ShowIcon : MonoBehaviour {
 		}
 		
 		if(Time.time < timer1){
+			if(!Icon1.GetComponent<Renderer>().enabled){
+				if(PlayerController.GetComponent<P_Movement> ().P1Direction == "x+"){
+					GameObject.Find("Player1").transform.rotation = Quaternion.Euler(0, 180, 0);
+				} else {
+					GameObject.Find("Player1").transform.rotation = Quaternion.Euler(0, 270, 0);
+					highlight1.transform.rotation = Quaternion.Euler(0, 90, 90);
+				}
+			}
+			highlight1.GetComponent<Renderer>().enabled = true;
 			Icon1.GetComponent<Renderer>().enabled = true;
-			Icon2.GetComponent<Renderer>().enabled = true;
 		} else {
+			highlight1.GetComponent<Renderer>().enabled = false;
 			Icon1.GetComponent<Renderer>().enabled = false;
-			Icon2.GetComponent<Renderer>().enabled = false;
 		}
 		if(Time.time < timer2){
-			Icon3.GetComponent<Renderer>().enabled = true;
-			Icon4.GetComponent<Renderer>().enabled = true;
+			if(!Icon2.GetComponent<Renderer>().enabled){
+				if(PlayerController.GetComponent<P_Movement> ().P2Direction == "x+"){
+					GameObject.Find("Player2").transform.rotation = Quaternion.Euler(0, 180, 0);
+				} else {
+					GameObject.Find("Player2").transform.rotation = Quaternion.Euler(0, 0, 0);
+					highlight2.transform.rotation = Quaternion.Euler(0, 0, 90);
+				}
+			}
+			highlight2.GetComponent<Renderer>().enabled = true;
+			Icon2.GetComponent<Renderer>().enabled = true;
 		} else {
-			Icon3.GetComponent<Renderer>().enabled = false;
-			Icon4.GetComponent<Renderer>().enabled = false;
+			highlight2.GetComponent<Renderer>().enabled = false;
+			Icon2.GetComponent<Renderer>().enabled = false;
 		}
 		if(Time.time < timer3){
-			Icon5.GetComponent<Renderer>().enabled = true;
-			Icon6.GetComponent<Renderer>().enabled = true;
+			if(!Icon3.GetComponent<Renderer>().enabled){
+				if(PlayerController.GetComponent<P_Movement> ().P3Direction == "x+"){
+					GameObject.Find("Player3").transform.rotation = Quaternion.Euler(0, 180, 0);
+				} else {
+					GameObject.Find("Player3").transform.rotation = Quaternion.Euler(0, 90, 0);
+					highlight3.transform.rotation = Quaternion.Euler(0, 270, 90);
+				}
+			}
+			highlight3.GetComponent<Renderer>().enabled = true;
+			Icon3.GetComponent<Renderer>().enabled = true;
 		} else {
-			Icon5.GetComponent<Renderer>().enabled = false;
-			Icon6.GetComponent<Renderer>().enabled = false;
+			highlight3.GetComponent<Renderer>().enabled = false;
+			Icon3.GetComponent<Renderer>().enabled = false;
+		}
+		
+		player1 = GameObject.Find("Player1").transform.position;
+		player2 = GameObject.Find("Player2").transform.position;
+		player3 = GameObject.Find("Player3").transform.position;
+		if(PlayerController.GetComponent<P_Movement> ().P1Direction == "x+"){
+			highlight1.transform.position = new Vector3(player1.x-0.2f, player1.y+0.2f, player1.z);
+		} else {
+			highlight1.transform.position = new Vector3(player1.x, player1.y+0.2f, player1.z+0.2f);
+		}
+		if(PlayerController.GetComponent<P_Movement> ().P2Direction == "x+"){
+			highlight2.transform.position = new Vector3(player2.x-0.2f, player2.y+0.2f, player2.z);
+		} else {
+			highlight2.transform.position = new Vector3(player2.x+0.2f, player2.y+0.2f, player2.z);
+		}
+		if(PlayerController.GetComponent<P_Movement> ().P2Direction == "x+"){
+			highlight3.transform.position = new Vector3(player3.x-0.2f, player3.y+0.2f, player3.z);
+		} else {
+			highlight3.transform.position = new Vector3(player3.x, player3.y+0.2f, player3.z-0.2f);
 		}
 	}
 	
